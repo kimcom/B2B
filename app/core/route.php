4bb5f7e0d93650915c3b141ee1c58bd2d4c34c1a
Егор <@NOUT-EGOR>
2016-01-12 15:35:51 +0200

    егор 2016-01-12 15:35

--- app/core/route.php
+++ app/core/route.php
@@ -1,25 +1,15 @@
 <?php
 class Route {
 	static function start() {
-//Fn::debugToLog("REQUEST_URI", urldecode($_SERVER['REQUEST_URI']));
-		if (1 == 0) {
-			ob_start();
-			var_dump($_REQUEST);
-			//var_dump($_SERVER);
-			//var_dump($_POST);
-			Fn::DebugToLog("test param\n" . $_SERVER['SCRIPT_FILENAME'] . "\n" . $_SERVER['REQUEST_URI'] . "\n", ob_get_clean());
-			ob_end_clean();
-		}
 		$pos = strpos($_SERVER['REQUEST_URI'], 'favicon.ico');
 		if ($pos !== FALSE)
 			Route::toFavicon();
-
-//Fn::debugToLog("route REQUEST_URI ", urldecode($_SERVER['REQUEST_URI']));
-//Fn::debugToLog("route QUERY_STRING", urldecode($_SERVER['QUERY_STRING']));
+			
 		// контроллер и действие по умолчанию
 		$controller_name = 'main';
 		$action_name = 'index';
-		// получаем имя контроллера
+		
+		// получаем имя контроллера	
 		$routes = explode('/', $_SERVER['REQUEST_URI']);
 		if (!empty($routes[1])) {
 			$controller_name = $routes[1];
@@ -27,6 +17,7 @@ class Route {
 			if ($pos !== FALSE)
 				$controller_name = substr($controller_name, 0, $pos);
 		}
+		
 		// получаем имя экшена
 		if (!empty($routes[2])) {
 			$action_name = $routes[2];
@@ -34,9 +25,10 @@ class Route {
 			if ($pos !== FALSE)
 				$action_name = substr($action_name, 0, $pos);
 		}
+		
 		//проверяем есть ли доступ к системе
-//Fn::debugToLog("test", 'access='.$_SESSION['access'].' $controller_name='.$controller_name);
-//$_SESSION['access'] = true;
+		//Fn::debugToLog("test", 'access='.$_SESSION['access'].' $controller_name='.$controller_name);
+		//$_SESSION['access'] = true;
 		if ($controller_name == 'main' && $action_name == 'index') {
 			Fn::redirectToControllerAndAction('main', 'cabinet_A0');
 			return;
